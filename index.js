2d865a4c81e575aeb9765e7b73f7e2777b4dca3f
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-02-25 20:35:46 +0500

    dh

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const frontend_key = process.env.FRONTEND_KEY;
 
-app.use(
-  cors({
-    origin: frontend_key,
-  })
-);
+app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 const mongoDbKey = process.env.MONGODB_KEY;
